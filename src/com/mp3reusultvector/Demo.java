5573be9c9f64a128ac801e435da2ed2b711f1cb7
duyhoangta <duyhoangptit@gmail.com>
2016-04-22 22:00:47 +0700

    Initial commit

---
+++ src/com/mp3reusultvector/Demo.java
@@ -0,0 +1,12 @@
+package com.mp3reusultvector;
+
+/**
+ * author HoangPtit
+ * Date 4/22/2016
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Mp3 Cosin Result Vector");
+    }
+}
